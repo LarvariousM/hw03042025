636e003bfd8337aed79f561c0f1e983922e66815
LarvariousM <larvariousmcdonald@gmail.com>
2025-03-02 14:21:54 -0600

    modified key pair

--- 2-S3Bucket.tf
+++ 2-S3Bucket.tf
@@ -2,7 +2,7 @@
 terraform {
     backend "s3" {
         bucket = "databases3"     #unique name of the s3 buckect  
-        key = "MyLinuxBox"            #name of the file 
+        key = "LinuxBoxforJenkins"            #name of the file 
         region = "us-east-1"        #region of the s3 bucket
 }
 }
