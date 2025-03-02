966da4387962049b1fc188f5e8338a73b522100a
LarvariousM <larvariousmcdonald@gmail.com>
2025-03-02 14:15:58 -0600

    commit first repo

---
+++ 2-S3Bucket.tf
@@ -0,0 +1,11 @@
+
+terraform {
+    backend "s3" {
+        bucket = "databases3"     #unique name of the s3 buckect  
+        key = "MyLinuxBox"            #name of the file 
+        region = "us-east-1"        #region of the s3 bucket
+}
+}
+
+
+
